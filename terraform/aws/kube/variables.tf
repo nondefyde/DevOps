ea5285e5801d96bf4378c325476ab8a34e5584d1
Emmanuel Okafor <nondefyde@gmail.com>
2023-01-20 13:38:49 +0100

    removed unused variables

--- terraform/aws/kube/variables.tf
+++ terraform/aws/kube/variables.tf
@@ -24,27 +24,3 @@ variable "namespace" {
   type = string
   description = "The deployment namespace"
 }
-
-variable "app_name" {
-  type        = string
-  description = "Deployment project name"
-}
-
-variable "image_name" {
-  type        = string
-  description = "Deployment image name"
-}
-
-variable "image_tag" {
-  type        = string
-  description = "Deployment image tag"
-}
-
-variable "app_port" {
-  type        = number
-  description = "Deployment container port"
-}
-variable "ingress_host" {
-  type        = string
-  description = "The ingress host address"
-}
