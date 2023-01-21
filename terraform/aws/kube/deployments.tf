c9b8a4fe692a6424b416bcabac419fef9a889cbb
Emmanuel Okafor <nondefyde@gmail.com>
2023-01-21 17:05:50 +0100

    clean up kube

--- terraform/aws/kube/deployments.tf
+++ terraform/aws/kube/deployments.tf
@@ -41,58 +41,58 @@ module "lb_role" {
     }
   }
 }
-
-resource "kubernetes_service_account" "service-account" {
-  metadata {
-    name = "aws-load-balancer-controller"
-    namespace = "kube-system"
-    labels = {
-      "app.kubernetes.io/name"= "aws-load-balancer-controller"
-      "app.kubernetes.io/component"= "controller"
-    }
-    annotations = {
-      "eks.amazonaws.com/role-arn" = module.lb_role.arn
-      "eks.amazonaws.com/sts-regional-endpoints" = "true"
-    }
-  }
-}
-
-resource "helm_release" "lb" {
-  name       = "aws-load-balancer-controller"
-  repository = "https://aws.github.io/eks-charts"
-  chart      = "aws-load-balancer-controller"
-  namespace  = "kube-system"
-  depends_on = [
-    kubernetes_service_account.service-account
-  ]
-
-  set {
-    name  = "region"
-    value = var.aws_region
-  }
-
-  set {
-    name  = "vpcId"
-    value = module.module_vpc.vpc_id
-  }
-
-  set {
-    name  = "image.repository"
-    value = "602401143452.dkr.ecr.eu-west-2.amazonaws.com/amazon/aws-load-balancer-controller"
-  }
-
-  set {
-    name  = "serviceAccount.create"
-    value = "false"
-  }
-
-  set {
-    name  = "serviceAccount.name"
-    value = "aws-load-balancer-controller"
-  }
-
-  set {
-    name  = "clusterName"
-    value = local.eks_cluster_name
-  }
-}
+#
+#resource "kubernetes_service_account" "service-account" {
+#  metadata {
+#    name = "aws-load-balancer-controller"
+#    namespace = "kube-system"
+#    labels = {
+#      "app.kubernetes.io/name"= "aws-load-balancer-controller"
+#      "app.kubernetes.io/component"= "controller"
+#    }
+#    annotations = {
+#      "eks.amazonaws.com/role-arn" = module.lb_role.arn
+#      "eks.amazonaws.com/sts-regional-endpoints" = "true"
+#    }
+#  }
+#}
+#
+#resource "helm_release" "lb" {
+#  name       = "aws-load-balancer-controller"
+#  repository = "https://aws.github.io/eks-charts"
+#  chart      = "aws-load-balancer-controller"
+#  namespace  = "kube-system"
+#  depends_on = [
+#    kubernetes_service_account.service-account
+#  ]
+#
+#  set {
+#    name  = "region"
+#    value = var.aws_region
+#  }
+#
+#  set {
+#    name  = "vpcId"
+#    value = module.module_vpc.vpc_id
+#  }
+#
+#  set {
+#    name  = "image.repository"
+#    value = "602401143452.dkr.ecr.eu-west-2.amazonaws.com/amazon/aws-load-balancer-controller"
+#  }
+#
+#  set {
+#    name  = "serviceAccount.create"
+#    value = "false"
+#  }
+#
+#  set {
+#    name  = "serviceAccount.name"
+#    value = "aws-load-balancer-controller"
+#  }
+#
+#  set {
+#    name  = "clusterName"
+#    value = local.eks_cluster_name
+#  }
+#}
