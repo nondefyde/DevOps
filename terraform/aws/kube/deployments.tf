dcaf903ca9827d618b95773e6fdccadb45f4aa7d
Emmanuel Okafor <nondefyde@gmail.com>
2023-01-21 15:44:04 +0100

    clean up kube

--- terraform/aws/kube/deployments.tf
+++ terraform/aws/kube/deployments.tf
@@ -3,6 +3,7 @@ data "aws_eks_cluster_auth" "cluster" {
 }
 
 provider "kubernetes" {
+  alias                  = "eks"
   host                   = module.aws_eks_cluster.cluster_endpoint
   cluster_ca_certificate = base64decode(module.aws_eks_cluster.kubeconfig_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.cluster.token
@@ -24,20 +25,35 @@ provider "helm" {
   }
 }
 
-module "nginx-controller" {
-  source  = "terraform-iaac/nginx-controller/helm"
+module "alb_ingress_controller" {
+  source  = "iplabs/alb-ingress-controller/kubernetes"
+  version = "3.1.0"
 
-  additional_set = [
-    {
-      name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
-      value = "nlb"
-      type  = "string"
-    },
-    {
-      name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-cross-zone-load-balancing-enabled"
-      value = "true"
-      type  = "string"
-    }
-  ]
+  providers = {
+    kubernetes = "kubernetes.eks"
+  }
+
+  k8s_cluster_type = "eks"
+  k8s_namespace    = "kube-system"
+
+  aws_region_name  = data.aws_region.current.name
+  k8s_cluster_name = data.aws_eks_cluster.target.name
 }
+
+#module "nginx-controller" {
+#  source = "terraform-iaac/nginx-controller/helm"
+#
+#  additional_set = [
+#    {
+#      name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
+#      value = "nlb"
+#      type  = "string"
+#    },
+#    {
+#      name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-cross-zone-load-balancing-enabled"
+#      value = "true"
+#      type  = "string"
+#    }
+#  ]
+#}
 #--------------------------------------------------------
