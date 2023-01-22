c846e8e3fba584bf0c8ce37070f321bae6aa8d59
Emmanuel Okafor <nondefyde@gmail.com>
2023-01-22 01:20:42 +0100

    added file

--- terraform/aws/kube/deployments.tf
+++ terraform/aws/kube/deployments.tf
@@ -1,62 +1,62 @@
-#data "aws_eks_cluster_auth" "cluster" {
-#  name = local.eks_cluster_name
-#}
-#
-#provider "kubernetes" {
-#  host                   = module.aws_eks_cluster.cluster_endpoint
-#  cluster_ca_certificate = base64decode(module.aws_eks_cluster.kubeconfig_certificate_authority_data)
-#  token                  = data.aws_eks_cluster_auth.cluster.token
-#}
-#
-#
-#data "tls_certificate" "cert" {
-#  url = module.aws_eks_cluster.eks_oidc_url
-#}
-#
-#resource "aws_iam_openid_connect_provider" "openid_connect_provider" {
-#  client_id_list  = ["sts.amazonaws.com"]
-#  thumbprint_list = [data.tls_certificate.cert.certificates.0.sha1_fingerprint]
-#  url             = module.aws_eks_cluster.eks_oidc_url
-#}
-#
-#module "aws_lb" {
-#  source           = "./modules/aws_lb"
-#  account_id       = data.aws_caller_identity.current.account_id
-#  eks_cluster_name = local.eks_cluster_name
-#  oidc_host_path   = module.aws_eks_cluster.eks_oidc_url
-#  region           = var.aws_region
-#  vpc_id           = module.module_vpc.vpc_id
-#
-#  depends_on = [aws_iam_openid_connect_provider.openid_connect_provider]
-#}
-#
-#provider "kubectl" {
-#  host                   = module.aws_eks_cluster.cluster_endpoint
-#  cluster_ca_certificate = base64decode(module.aws_eks_cluster.kubeconfig_certificate_authority_data)
-#  token                  = data.aws_eks_cluster_auth.cluster.token
-#  load_config_file       = false
-#}
-#
-#
-#resource "kubectl_manifest" "ingessclassparams" {
-#  yaml_body = file("${path.module}/yamls/ingressclassparams.yaml")
-#
-#  wait = true
-#}
-#
-#resource "kubectl_manifest" "targetgroupbindings" {
-#  yaml_body = file("${path.module}/yamls/targetgroupbindings.yaml")
-#
-#  wait = true
-#}
-#
-#provider "helm" {
-#  kubernetes {
-#    host                   = module.aws_eks_cluster.cluster_endpoint
-#    cluster_ca_certificate = base64decode(module.aws_eks_cluster.kubeconfig_certificate_authority_data)
-#    token                  = data.aws_eks_cluster_auth.cluster.token
-#  }
-#}
+data "aws_eks_cluster_auth" "cluster" {
+  name = local.eks_cluster_name
+}
+
+provider "kubernetes" {
+  host                   = module.aws_eks_cluster.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.aws_eks_cluster.kubeconfig_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+
+data "tls_certificate" "cert" {
+  url = module.aws_eks_cluster.eks_oidc_url
+}
+
+resource "aws_iam_openid_connect_provider" "openid_connect_provider" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.cert.certificates.0.sha1_fingerprint]
+  url             = module.aws_eks_cluster.eks_oidc_url
+}
+
+module "aws_lb" {
+  source           = "./modules/aws_lb"
+  account_id       = data.aws_caller_identity.current.account_id
+  eks_cluster_name = local.eks_cluster_name
+  oidc_host_path   = module.aws_eks_cluster.eks_oidc_url
+  region           = var.aws_region
+  vpc_id           = module.module_vpc.vpc_id
+
+  depends_on = [aws_iam_openid_connect_provider.openid_connect_provider]
+}
+
+provider "kubectl" {
+  host                   = module.aws_eks_cluster.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.aws_eks_cluster.kubeconfig_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+}
+
+
+resource "kubectl_manifest" "ingessclassparams" {
+  yaml_body = file("${path.module}/yamls/ingressclassparams.yaml")
+
+  wait = true
+}
+
+resource "kubectl_manifest" "targetgroupbindings" {
+  yaml_body = file("${path.module}/yamls/targetgroupbindings.yaml")
+
+  wait = true
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.aws_eks_cluster.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.aws_eks_cluster.kubeconfig_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
 #
 ## V 2.4.1
 ## https://kubernetes-sigs.github.io/aws-load-balancer-controller/latest/deploy/installation/
