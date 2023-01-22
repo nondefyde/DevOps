e6c5b227bdad59659679d108d287401ee6702947
Emmanuel Okafor <nondefyde@gmail.com>
2023-01-22 19:42:25 +0100

    added file

--- terraform/aws/_scripts/prepare.sh
+++ terraform/aws/_scripts/prepare.sh
@@ -3,7 +3,7 @@
 cd ../${1}
 echo "Environment AWS: ${1} - ${2}"
 mkdir -p _state
-terraform providers lock -platform=windows_amd64 -platform=darwin_amd64 -platform=linux_amd64
+#terraform providers lock -platform=windows_amd64 -platform=darwin_amd64 -platform=linux_amd64
 terraform init -input=false -migrate-state \
 -backend-config="bucket=${2}-tfstate" \
 -backend-config="region=us-west-2" \
