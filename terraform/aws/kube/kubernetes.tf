723bba73f2d3a92fce7a03840b3cd15f977dde8c
Emmanuel Okafor <nondefyde@gmail.com>
2023-01-23 02:07:01 +0100

    added variable

--- terraform/aws/kube/kubernetes.tf
+++ terraform/aws/kube/kubernetes.tf
@@ -1,4 +1,4 @@
-resource "kubectl_manifest" "targetgroupbindings" {
+resource "kubectl_manifest" "nginx-ingress-controller" {
   yaml_body = file("${path.module}/yamls/nginx.yaml")
 
   wait = true
--- terraform/aws/kube/provider.tf
+++ terraform/aws/kube/provider.tf
@@ -28,3 +28,10 @@ provider "helm" {
     token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
+
+provider "kubectl" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+}
