73f34e71ed46d0d741c38b2bea1400fbd546c43b
Emmanuel Okafor <nondefyde@gmail.com>
2023-01-23 02:15:49 +0100

    added variable

--- terraform/aws/kube/main.tf
+++ terraform/aws/kube/main.tf
@@ -44,15 +44,3 @@ resource "aws_ecr_repository" "app_registry" {
   }
   force_delete = true
 }
-
-#module "load_balancer_controller" {
-#  source = "git::https://github.com/DNXLabs/terraform-aws-eks-lb-controller.git"
-#
-#  enabled = true
-#
-#  cluster_identity_oidc_issuer     = module.eks_cluster.cluster_oidc_issuer_url
-#  cluster_identity_oidc_issuer_arn = module.eks_cluster.oidc_provider_arn
-#  cluster_name                     = module.eks_cluster.cluster_id
-#
-#  depends_on = [module.eks_cluster]
-#}
