169325a5a692fb70497ce9fe62014ea2ab9d1577
Emmanuel Okafor <nondefyde@gmail.com>
2023-01-25 14:23:09 +0100

    added secret build

---
+++ scripts/base64.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+echo File name $1
+
+#!/usr/bin/env bash
+if [[ "${OSTYPE}" = darwin* ]]; then
+  # OSX
+  if [ -t 0 ]; then
+    base64 "$@"
+  else
+    cat /dev/stdin | base64 "$@"
+  fi
+else
+  # Linux
+  if [ -t 0 ]; then
+    base64 -w 0 "$@"
+  else
+    cat /dev/stdin | base64 -w 0 "$@"
+  fi
+fi
