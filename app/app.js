3240c2e3c412c3ab1a5d24db463fcd9f4d622863
Emmanuel Okafor <nondefyde@gmail.com>
2023-01-25 20:15:25 +0100

    added build command

--- app/app.js
+++ app/app.js
@@ -8,7 +8,7 @@ app.get('/v1/ping', (req, res) => {
 })
 
 app.get('/', (req, res) => {
-	res.send('Hello World! I did it again');
+	res.send('Hello World! Deployed');
 })
 
 app.listen(port, () => {
