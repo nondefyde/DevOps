ed64333ee0daf31c894784b9d608d9f280c0c2aa
Emmanuel Okafor <nondefyde@gmail.com>
2023-01-28 19:27:18 +0100

    updated port and domain

--- terraform/azure/vm/vm.sh
+++ terraform/azure/vm/vm.sh
@@ -10,8 +10,10 @@ echo "I have network";
 sudo apt update
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 
-sudo apt install docker-compose
-#sudo apt-get install docker-compose-plugin
+#Install Docker compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+which docker-compose
 
 # Add Docker’s official GPG key
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
