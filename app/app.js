c74dbd7adff35cc3235f9265954ef84b7ab8ff25
Emmanuel Okafor <nondefyde@gmail.com>
2023-01-29 13:13:05 +0100

    dev ops scripts

--- app/app.js
+++ app/app.js
@@ -8,7 +8,7 @@ app.get('/v1/ping', (req, res) => {
 })
 
 app.get('/', (req, res) => {
-	res.send(`Hello World! Deployed image 6 now`);
+	res.send(`Hello World! Start again`);
 })
 
 app.listen(port, () => {
