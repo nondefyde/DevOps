7e06a693f38fef6c323eb2ae9d51c1058134e23f
Emmanuel Okafor <nondefyde@gmail.com>
2023-01-29 11:09:02 +0100

    updated port and domain

--- terraform/azure/vm/vm.sh
+++ terraform/azure/vm/vm.sh
@@ -11,6 +11,9 @@ sudo apt update
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 
 #Install Docker compose
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+sudo mkdir -p $DOCKER_CONFIG/cli-plugins
+sudo curl -SL https://github.com/docker/compose/releases/download/v2.15.2/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 which docker-compose
