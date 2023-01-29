ae2e337edea1cf2e4a2287918ed10b34282b9d2f
Emmanuel Okafor <nondefyde@gmail.com>
2023-01-29 13:18:26 +0100

    dev ops scripts

--- vm/deploy.sh
+++ vm/deploy.sh
@@ -6,7 +6,7 @@ ZERO=0
 
 echo "Number of container running image is ${IMAGE_COUNT}"
 
-if [ "$IMAGE_COUNT" -gt 0 ]; then
+if [ $IMAGE_COUNT -gt 0 ]; then
   NEWCOUNT=$((IMAGE_COUNT+1))
   echo "Spin up new container with updated image to scale up to ${NEWCOUNT}"
 
