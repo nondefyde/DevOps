f52dfc680e27ba3af6b48f8a6f5cad8215e63ad2
Emmanuel Okafor <nondefyde@gmail.com>
2023-01-29 10:26:55 +0100

    updated port and domain

--- vm/deploy.sh
+++ vm/deploy.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-IMAGE_COUNT=$(sudo docker ps | grep node-server:latest | wc -l)
-IMAGES=$(sudo docker ps --format '{{.Names}}')
+IMAGE_COUNT=$(sudo docker ps | grep $1 | wc -l)
+IMAGES=$(sudo docker ps --filter ancestor=$1 --format '{{.Names}}')
 ZERO=0
 
 echo "Number of container running image is ${IMAGE_COUNT}"
