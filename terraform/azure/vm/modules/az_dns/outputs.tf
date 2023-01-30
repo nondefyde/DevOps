cea3b714ed22bebe5dda22a6546709871447533d
Emmanuel Okafor <nondefyde@gmail.com>
2023-01-30 15:04:07 +0100

    added service

--- terraform/azure/vm/modules/az_dns/outputs.tf
+++ terraform/azure/vm/modules/az_dns/outputs.tf
@@ -6,6 +6,6 @@ output "dns_cname_record" {
   value     = cloudflare_record.cf_vm_cname_record.name
 }
 
-output "dns_www_cname_record" {
-  value     = cloudflare_record.cf_vm_www_record.name
-}
+#output "dns_www_cname_record" {
+#  value     = cloudflare_record.cf_vm_www_record.name
+#}
