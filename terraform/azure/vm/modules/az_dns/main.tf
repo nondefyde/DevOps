91bb889a8969259f0c585c63330d1f4720ab3791
Emmanuel Okafor <nondefyde@gmail.com>
2023-01-30 16:53:16 +0100

    fixed service env

--- terraform/azure/vm/modules/az_dns/main.tf
+++ terraform/azure/vm/modules/az_dns/main.tf
@@ -27,7 +27,7 @@ resource "cloudflare_record" "cf_vm_sub_domain_cname_record" {
   zone_id         = var.cloudflare_zone_id
   name            = "*.${var.service}"
   value           = var.public_ip_dns_name
-  type            = "TXT"
+  type            = "CNAME"
   allow_overwrite = true
 
   depends_on = [azurerm_dns_cname_record.vm_dns_record]
