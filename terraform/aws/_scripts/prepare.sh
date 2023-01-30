3ecf9a971900cf3361b639c37ec45c72e766cfc7
Emmanuel Okafor <nondefyde@gmail.com>
2023-01-30 22:39:12 +0100

    fixed region

--- terraform/aws/_scripts/prepare.sh
+++ terraform/aws/_scripts/prepare.sh
@@ -6,7 +6,7 @@ mkdir -p _state
 #terraform providers lock -platform=windows_amd64 -platform=darwin_amd64 -platform=linux_amd64
 terraform init -input=false -migrate-state \
 -backend-config="bucket=${2}-tfstate" \
--backend-config="region=us-west-2" \
+-backend-config="region=${3}" \
 -backend-config="key=${2}.terraform.tfstate"
 
 terraform plan -input=false -var-file=../../_env/${2}.aws.tfvars -out ./_state/${2}.tfplan
