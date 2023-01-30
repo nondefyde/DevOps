f5dad7bc6abb289690c97e3cf5257db9003bc455
Emmanuel Okafor <nondefyde@gmail.com>
2023-01-30 18:07:14 +0100

    fixed sub domain

--- terraform/azure/vm/modules/az_dns/main.tf
+++ terraform/azure/vm/modules/az_dns/main.tf
@@ -1,39 +1,3 @@
-resource "azurerm_dns_zone" "vm_dns_zone" {
-  name                = "dev.${var.dns_domain}"
-  resource_group_name = "${var.prefix}-group"
-}
-
-#resource "azurerm_dns_cname_record" "vm_dns_record" {
-#  name                = "*"
-#  zone_name           = azurerm_dns_zone.vm_dns_zone.name
-#  resource_group_name = "${var.prefix}-group"
-#  ttl                 = 300
-#  record              = var.public_ip_dns_name
-#  depends_on = [azurerm_dns_zone.vm_dns_zone]
-#}
-
-#resource "cloudflare_record" "cf_vm_www_record" {
-#  zone_id         = var.cloudflare_zone_id
-#  name            = "www"
-#  value           = var.dns_domain
-#  type            = "CNAME"
-#  proxied         = true
-#  allow_overwrite = true
-#
-#  depends_on = [azurerm_dns_cname_record.vm_dns_record]
-#}
-
-#resource "cloudflare_record" "cf_vm_a_record" {
-#  zone_id         = var.cloudflare_zone_id
-#  name            = "*"
-#  value           = var.public_ip
-#  type            = "A"
-#  proxied         = true
-#  allow_overwrite = true
-#
-#  depends_on =  [azurerm_dns_cname_record.vm_dns_record]
-#}
-
 resource "cloudflare_record" "cf_vm_subdomain_cname_record" {
   zone_id         = var.cloudflare_zone_id
   name            = "*.${var.service}"
@@ -41,8 +5,6 @@ resource "cloudflare_record" "cf_vm_subdomain_cname_record" {
   type            = "CNAME"
   proxied         = true
   allow_overwrite = true
-
-  depends_on = [azurerm_dns_zone.vm_dns_zone]
 }
 
 resource "cloudflare_record" "cf_vm_domain_a_record" {
@@ -53,5 +15,16 @@ resource "cloudflare_record" "cf_vm_domain_a_record" {
   proxied         = true
   allow_overwrite = true
 
-  depends_on =  [azurerm_dns_zone.vm_dns_zone]
+  depends_on =  [cloudflare_record.cf_vm_subdomain_cname_record]
+}
+
+resource "cloudflare_certificate_pack" "example" {
+  zone_id               = var.cloudflare_zone_id
+  type                  = "advanced"
+  hosts                 = [var.dns_domain, "*.${var.dns_domain}", "*.${var.service}.${var.dns_domain}"]
+  validation_method     = "txt"
+  validity_days         = 365
+  certificate_authority = "digicert"
+  cloudflare_branding   = false
+  wait_for_active_status = true
 }
