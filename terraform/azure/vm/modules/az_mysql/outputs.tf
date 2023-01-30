4eff2f7e15a74a11a85235941bde7f0c54931ffa
Emmanuel Okafor <nondefyde@gmail.com>
2023-01-30 23:27:29 +0100

    removed vpn id

--- terraform/azure/vm/modules/az_mysql/outputs.tf
+++ terraform/azure/vm/modules/az_mysql/outputs.tf
@@ -2,10 +2,6 @@ output "mysql_server_zone" {
   value     = azurerm_mysql_flexible_server.mysql_server.zone
 }
 
-output "mysql_server_connection" {
-  value     = azurerm_mysql_flexible_server.mysql_server.connection
-}
-
 output "mysql_server_name" {
   value     = azurerm_mysql_flexible_server.mysql_server.name
 }
