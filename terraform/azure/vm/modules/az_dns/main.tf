5efa8018f0d91a67fb8afa7f3ace18fe75c191cc
Emmanuel Okafor <nondefyde@gmail.com>
2023-02-03 08:01:04 +0100

    fixed storage account location

--- terraform/azure/vm/modules/az_dns/main.tf
+++ terraform/azure/vm/modules/az_dns/main.tf
@@ -7,17 +7,6 @@ resource "cloudflare_record" "cf_vm_subdomain_cname_record" {
   allow_overwrite = true
 }
 
-resource "cloudflare_record" "cf_vm_domain_a_record" {
-  zone_id         = var.cloudflare_zone_id
-  name            = var.dns_domain
-  value           = var.public_ip
-  type            = "A"
-  proxied         = true
-  allow_overwrite = true
-
-  depends_on =  [cloudflare_record.cf_vm_subdomain_cname_record]
-}
-
 resource "cloudflare_certificate_pack" "cf_vm_ssl_record" {
   zone_id               = var.cloudflare_zone_id
   type                  = "advanced"
