1e6dd46edf7d8bd12d802a1416fae82b428c314a
Emmanuel Okafor <nondefyde@gmail.com>
2023-02-06 16:26:45 +0100

    removed azzure login

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,6 +3,7 @@
 echo File name $1
 
 outputs=$(cat $1)
+echo $outputs
 for key in $(echo "${outputs}" | jq -r 'keys[]'); do
   KEY=$(echo "${key}")
   VALUE=$(echo "${outputs}" | jq -r ".${key}.value")
