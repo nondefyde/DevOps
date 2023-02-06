7d2edf8a4169a7d4504d4b7087e9d5b1e140f814
Emmanuel Okafor <nondefyde@gmail.com>
2023-02-06 20:10:53 +0100

    removed azzure login

---
+++ scripts/exenv.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo File name $1
+
+outputs=$(cat $1)
+for key in $(echo $outputs | jq -r 'keys[]'); do
+  KEY=$(echo $key | tr '[:lower:]' '[:upper:]')
+  VALUE=$(echo $outputs | jq -r ".${key}.value")
+  echo "key == $KEY"
+  echo "value == $VALUE"
+done
