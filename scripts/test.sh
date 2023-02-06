d1330bcb7f38839740eebb1f6e6ab8c67ff00def
Emmanuel Okafor <nondefyde@gmail.com>
2023-02-06 16:22:09 +0100

    removed azzure login

---
+++ scripts/test.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo File name $1
+
+outputs=$(cat $1)
+for key in $(echo "${outputs}" | jq -r 'keys[]'); do
+  KEY=$(echo "${key}")
+  VALUE=$(echo "${outputs}" | jq -r ".${key}.value")
+  echo "key == $KEY"
+  echo "value == $VALUE"
+  export TF_OUTPUT_$KEY=$VALUE
+done
