24a566d86c55cbaf9c0dbd4e8eed16e500894206
Emmanuel Okafor <nondefyde@gmail.com>
2023-02-08 00:45:26 +0100

    cleanups

--- tf/azure/vn/outputs.tf
+++ tf/azure/vn/outputs.tf
@@ -1,15 +1,15 @@
-output "app_virtual_network_name" {
+output "virtual_network_name" {
   value = azurerm_virtual_network.app_virtual_network.name
 }
 
-output "app_virtual_network_id" {
+output "virtual_network_id" {
   value = azurerm_virtual_network.app_virtual_network.id
 }
 
-output "app_virtual_subnet_name" {
+output "virtual_subnet_name" {
   value = azurerm_subnet.app_virtual_subnet.name
 }
 
-output "app_virtual_subnet_id" {
+output "virtual_subnet_id" {
   value = azurerm_subnet.app_virtual_subnet.id
 }
