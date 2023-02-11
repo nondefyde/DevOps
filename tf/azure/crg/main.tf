482931e096bb9ad6d064fc68e4f2c0e6ea55ef7a
Emmanuel Okafor <nondefyde@gmail.com>
2023-02-11 15:49:17 +0100

    fixed update

--- tf/azure/crg/main.tf
+++ tf/azure/crg/main.tf
@@ -2,3 +2,16 @@ resource "azurerm_resource_group" "rg" {
   name     = "${var.prefix}-group"
   location = var.location
 }
+
+resource "azurerm_container_registry" "vm_acr" {
+  name                = "${var.prefix}-group"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Premium"
+  admin_enabled       = false
+
+  georeplications {
+    location = "East US 2"
+  }
+  depends_on = [azurerm_resource_group.rg]
+}
