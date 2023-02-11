f5bf4e4bb9e045aa2175bc3f3643cf5496406393
Emmanuel Okafor <nondefyde@gmail.com>
2023-02-11 15:51:53 +0100

    fixed update

--- tf/azure/crg/main.tf
+++ tf/azure/crg/main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_container_registry" "vm_acr" {
-  name                = "${var.prefix}-group"
+  name                = "${var.prefix}acr"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Premium"
