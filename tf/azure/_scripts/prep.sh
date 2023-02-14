0177669cdd4176fd0a3be665af47617947d541de
Emmanuel Okafor <nondefyde@gmail.com>
2023-02-15 00:36:43 +0100

    added output

--- tf/azure/_scripts/prep.sh
+++ tf/azure/_scripts/prep.sh
@@ -22,26 +22,25 @@ DOCKER_COMPOSE_FILE=https://raw.githubusercontent.com/nondefyde/DevOps/main/ci/c
 curl -sSL "${DOCKER_COMPOSE_FILE}" | sed "s;{IMAGE};$3;g; s;{NODE_ENV};$4;g; s;{VIRTUAL_HOST};$5;g; s;{PORT};$6;g;"  > "/home/${7}/vm/docker-compose.yml"
 
 
-#echo "Login to container registry ${1}acr"
-#LOGIN_SERVER=$(az acr login -n ${1}acr --expose-token)
-#accessToken=$( jq -r  '.accessToken' <<< "${LOGIN_SERVER}" )
-#server=$( jq -r  '.loginServer' <<< "${LOGIN_SERVER}" )
-#
-#echo "Logged in docker to server >> ${server}"
-#sudo docker login ${server} --username 00000000-0000-0000-0000-000000000000 --password ${accessToken}
-#
-#echo "Check if reverse proxy is running"
-#IMAGE_COUNT=$(sudo docker ps --filter="name=reverse_proxy" | grep reverse_proxy | wc -l)
-#ZERO=0
-#echo "IMAGE_COUNT ${IMAGE_COUNT}"
-#if [ $IMAGE_COUNT -gt 0 ]; then
-#  echo "Reverse Proxy present"
-##  curl s https://raw.githubusercontent.com/nondefyde/DevOps/main/tf/azure/_scripts/deploy.sh
-##  chmod a+x ./deploy.sh
-##  ./deploy.sh $2
-#else
-#  sudo docker pull jwilder/nginx-proxy:latest
-#  sudo docker run -d -p 80:80 -v /var/run/docker.sock:/tmp/docker.sock:ro --name reverse_proxy --net nginx-proxy jwilder/nginx-proxy
-#fi
-#
+echo "Login to container registry ${1}acr"
+LOGIN_SERVER=$(az acr login -n ${1}acr --expose-token)
+accessToken=$( jq -r  '.accessToken' <<< "${LOGIN_SERVER}" )
+server=$( jq -r  '.loginServer' <<< "${LOGIN_SERVER}" )
+sudo docker login ${server} --username 00000000-0000-0000-0000-000000000000 --password ${accessToken}
+echo "Logged in docker to server >> ${server}"
+
+echo "Check if reverse proxy is running"
+IMAGE_COUNT=$(sudo docker ps --filter="name=reverse_proxy" | grep reverse_proxy | wc -l)
+ZERO=0
+echo "IMAGE_COUNT ${IMAGE_COUNT}"
+if [ $IMAGE_COUNT -gt 0 ]; then
+  echo "Reverse Proxy present"
+#  curl -sSL https://raw.githubusercontent.com/nondefyde/DevOps/main/tf/azure/_scripts/deploy.sh
+#  chmod a+x ./deploy.sh
+#  ./deploy.sh $2
+else
+  sudo docker pull jwilder/nginx-proxy:latest
+  sudo docker run -d -p 80:80 -v /var/run/docker.sock:/tmp/docker.sock:ro --name reverse_proxy --net nginx-proxy jwilder/nginx-proxy
+fi
+
 
