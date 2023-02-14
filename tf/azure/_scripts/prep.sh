8f78c02c7d80431e9e591534f7ac7d32f75080bf
Emmanuel Okafor <nondefyde@gmail.com>
2023-02-15 00:24:23 +0100

    added output

--- tf/azure/_scripts/prep.sh
+++ tf/azure/_scripts/prep.sh
@@ -18,7 +18,7 @@ touch "/home/${7}/vm/.env"
 DECODED=$(echo "${2}" | base64 --decode > /home/adminuser/vm/.env)
 
 echo "Generate docker compose file"
-DOCKER_COMPOSE_FILE=https://raw.githubusercontent.com/nondefyde/DevOps/main/ci/docker-compose.yml
+DOCKER_COMPOSE_FILE=https://raw.githubusercontent.com/nondefyde/DevOps/main/ci/compose.tpl
 curl -sSL "${DOCKER_COMPOSE_FILE}" | sed "s;{IMAGE};$3;g; s;{NODE_ENV};$4;g; s;{VIRTUAL_HOST};$5;g; s;{PORT};$6;g;"  > "/home/${7}/vm/docker-compose.yml"
 
 
