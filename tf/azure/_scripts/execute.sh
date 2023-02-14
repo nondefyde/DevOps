53b56f4c77e7749b0b352bfdd8801922e9e6d3bc
Emmanuel Okafor <nondefyde@gmail.com>
2023-02-14 11:56:40 +0100

    added output

--- tf/azure/_scripts/execute.sh
+++ tf/azure/_scripts/execute.sh
@@ -3,8 +3,19 @@
 
 echo "Run Deployment"
 
-az vm run-command invoke
-  --command-id RunShellScript
-  --name ${{ env.project }}-${{ matrix.items }}-vm-1
-  --resource-group ${{ env.project }}-group
-  --scripts "curl -s https://raw.githubusercontent.com/nondefyde/DevOps/main/tf/azure/_scripts/prep.sh | bash -s $1 $2 $3"
+echo "Project     : ${1}"
+echo "Image       : ${2}"
+echo "App Secret  : ${3}"
+echo "Vm Name     : ${4}"
+echo "Vm Count    : ${5}"
+
+
+for i in $(seq 1 ${5}); do
+    az vm run-command invoke
+      --command-id RunShellScript
+      --name ${1}-${4}-vm-$i
+      --resource-group ${{ env.project }}-group
+      --scripts "curl -s https://raw.githubusercontent.com/nondefyde/DevOps/main/tf/azure/_scripts/prep.sh | bash -s $1 $2 $3"
+done
+
+
