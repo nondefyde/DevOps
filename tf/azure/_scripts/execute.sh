c43c225dfaf8bb4db64af4fd61789d7b176dcb26
Emmanuel Okafor <nondefyde@gmail.com>
2023-02-14 18:13:48 +0100

    added output

--- tf/azure/_scripts/execute.sh
+++ tf/azure/_scripts/execute.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-
 echo "Run Deployment"
 
 echo "Project     : ${1}"
@@ -21,6 +20,21 @@ az vm run-command invoke \
     ' \
   --parameters ${CONTAINER_NAME}
 
+echo "Login to docker"
+echo "Login to container registry ${1}acr"
+LOGIN_SERVER=$(az acr login -n ${1}acr --expose-token)
+accessToken=$( jq -r  '.accessToken' <<< "${LOGIN_SERVER}" )
+server=$( jq -r  '.loginServer' <<< "${LOGIN_SERVER}" )
+echo "logged in to server > ${server}"
+az vm run-command invoke \
+  -g ${RESOURCE_GROUP_NAME} \
+  -n ${VM_NAME} \
+  --command-id RunShellScript \
+  --scripts 'sudo docker login $1 --username 00000000-0000-0000-0000-000000000000 --password $2' \
+  --parameters ${server} ${accessToken}
+
+
+echo "Deploy to VM"
 for i in $(seq 1 ${5}); do
   echo "Run Command on VM ${1}-${4}-vm-$i"
   az vm run-command invoke \
