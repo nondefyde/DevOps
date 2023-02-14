76645759497b6a43649b692a78cec2c69f18a30d
Emmanuel Okafor <nondefyde@gmail.com>
2023-02-14 10:45:24 +0100

    added output

--- tf/azure/depl/main.tf
+++ tf/azure/depl/main.tf
@@ -2,13 +2,8 @@ data "azurerm_resource_group" "rg" {
   name = var.group
 }
 
-data "azurerm_network_interface" "vm_interface" {
-  count               = var.vm_count
-  name                = "${var.prefix}-${var.vm_name}-net-${count.index}"
-  resource_group_name = "existing-group"
-}
 
-data "azurerm_linux_virtual_machine" "vms" {
+data "azurerm_virtual_machine" "vms" {
   count               = var.vm_count
   name                = "${var.prefix}-${var.name}-vm-${count.index + 1}"
   resource_group_name = data.azurerm_resource_group.rg.name
@@ -17,7 +12,7 @@ data "azurerm_linux_virtual_machine" "vms" {
 resource "azurerm_virtual_machine_extension" "deployment" {
   count               = var.vm_count
   name                 = "${var.prefix}-deployment"
-  virtual_machine_id  = element(data.azurerm_linux_virtual_machine.vms.*.id, count.index)
+  virtual_machine_id  = element(data.azurerm_virtual_machine.vms.*.id, count.index)
   publisher           = "Microsoft.Azure.Extensions"
   type                 = "CustomScript"
   type_handler_version = "2.0"
---
+++ tf/azure/depl/outputs.tf
@@ -0,0 +1,3 @@
+output "virtual_network_name" {
+  value = data.azurerm_resource_group.rg.name
+}
