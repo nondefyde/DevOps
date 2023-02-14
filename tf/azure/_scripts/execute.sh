2f48dc3ac6bdb97522d612e8c3be662d882dc3ab
Emmanuel Okafor <nondefyde@gmail.com>
2023-02-14 12:17:15 +0100

    added output

--- tf/azure/_scripts/execute.sh
+++ tf/azure/_scripts/execute.sh
@@ -11,11 +11,11 @@ echo "Vm Count    : ${5}"
 
 
 for i in $(seq 1 ${5}); do
-    az vm run-command invoke
-      --command-id RunShellScript
-      --name ${1}-${4}-vm-$i
-      --resource-group ${{ env.project }}-group
-      --scripts "curl -s https://raw.githubusercontent.com/nondefyde/DevOps/main/tf/azure/_scripts/prep.sh | bash -s $1 $2 $3"
+  az vm run-command invoke \
+    --command-id RunShellScript \
+    --name ${1}-${4}-vm-$i \
+    --resource-group ${1}-group \
+    --scripts "curl -s https://raw.githubusercontent.com/nondefyde/DevOps/main/tf/azure/_scripts/prep.sh | bash -s $1 $2 $3"
 done
 
 
