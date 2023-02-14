15e373422f14519a0704dfe1f774ca40f3926784
Emmanuel Okafor <nondefyde@gmail.com>
2023-02-14 19:17:37 +0100

    added output

--- tf/azure/_scripts/execute.sh
+++ tf/azure/_scripts/execute.sh
@@ -2,25 +2,34 @@
 
 echo "Run Deployment"
 
-echo "Project     : ${1}"
-echo "Image       : ${2}"
-echo "App Secret  : ${3}"
-echo "Vm Name     : ${4}"
-echo "Vm Count    : ${5}"
+echo "Client    : ${1}"
+echo "Secret    : ${2}"
+echo "Renant    : ${3}"
 
-RESOURCE_GROUP_NAME=${1}-group
-IMAGE=${2}
-APP_SECRET=${3}
-VM_NAME=${4}
-VM_COUNT=${5}
+echo "Project     : ${4}"
+echo "Image       : ${5}"
+echo "App Secret  : ${6}"
+echo "Vm Name     : ${7}"
+echo "Vm Count    : ${8}"
 
-for i in $(seq 1 ${5}); do
-  echo "Run Command on VM ${1}-${4}-vm-$i"
+
+RESOURCE_GROUP_NAME=${4}-group
+IMAGE=${5}
+APP_SECRET=${6}
+VM_NAME=${7}
+VM_COUNT=${8}
+
+for i in $(seq 1 ${8}); do
+  echo "Run Command on VM ${4}-${7}-vm-$i"
   az vm run-command invoke \
     --command-id RunShellScript \
-    --name ${1}-${4}-vm-$i \
+    --name ${4}-${7}-vm-$i \
     --resource-group ${RESOURCE_GROUP_NAME} \
-    --scripts "curl -s https://raw.githubusercontent.com/nondefyde/DevOps/main/tf/azure/_scripts/prep.sh | bash -s $1 $2 $3"
+    --scripts '
+         az login --service-principal --username ${1} --password ${2} --tenant ${3}
+         curl -s https://raw.githubusercontent.com/nondefyde/DevOps/main/tf/azure/_scripts/prep.sh | bash -s $4 $5 $6
+      ' \
+    --parameters ${1} ${2} ${3} ${4} ${5} ${6}
 done
 
 
