d71dbb302fc8fc9430e4b8459359704810d429f6
Emmanuel Okafor <nondefyde@gmail.com>
2023-02-15 03:23:57 +0100

    added output

--- tf/azure/_scripts/execute.sh
+++ tf/azure/_scripts/execute.sh
@@ -58,25 +58,17 @@ for i in $(seq 1 ${8}); do
     --resource-group ${4}-group \
     --scripts "curl -s ${PREP_SCRIPT} | bash -s ${PROJECT} ${APP_SECRET} ${IMAGE} ${ENV} ${VIRTUAL_HOST} ${PORT} ${VM_USER}"
 
-#  echo "Login to docker"
+#  echo "Deploy Update on VM ${4}-${7}-vm-$i"
 #  az vm run-command invoke \
+#    --command-id RunShellScript \
 #    --name ${4}-${7}-vm-$i \
 #    --resource-group ${4}-group \
-#    --command-id RunShellScript \
-#    --scripts 'sudo docker login $1 --username 00000000-0000-0000-0000-000000000000 --password $2' \
-#    --parameters "${server}" "${accessToken}"
-
-  echo "Deploy Update on VM ${4}-${7}-vm-$i"
-  az vm run-command invoke \
-    --command-id RunShellScript \
-    --name ${4}-${7}-vm-$i \
-    --resource-group ${4}-group \
-    --scripts '
-      echo "Login docker"
-      sudo docker login $1 --username 00000000-0000-0000-0000-000000000000 --password $2
-      cd vm
-      chmod +x deploy.sh
-      ./deploy.sh $3 $4 $5 $6 $7
-    ' \
-    --parameters "${server}" "${accessToken}" "${PROJECT}" "${IMAGE}" "${INSTANCE}" "${VM_USER}" "vm-app-"
+#    --scripts '
+#      echo "Login docker"
+#      sudo docker login $1 --username 00000000-0000-0000-0000-000000000000 --password $2
+#      cd vm
+#      chmod +x deploy.sh
+#      ./deploy.sh $3 $4 $5 $6 $7
+#    ' \
+#    --parameters "${server}" "${accessToken}" "${PROJECT}" "${IMAGE}" "${INSTANCE}" "${VM_USER}" "vm-app-"
 done
