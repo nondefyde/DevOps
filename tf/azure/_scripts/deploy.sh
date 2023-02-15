523ac21df779802fff4e4fb201150b36f314124a
Emmanuel Okafor <nondefyde@gmail.com>
2023-02-15 04:15:47 +0100

    added output

--- tf/azure/_scripts/deploy.sh
+++ tf/azure/_scripts/deploy.sh
@@ -6,6 +6,7 @@ echo "instance    : $3"
 echo "Vm user     : $4"
 echo "App alias   : $5"
 
+echo "Use docker rootless"
 docker context use rootless
 
 echo "Remove unused images as part of cleaning up"
