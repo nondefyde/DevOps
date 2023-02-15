227b00a80a7d314ede552a25c64746e372cd70b4
Emmanuel Okafor <nondefyde@gmail.com>
2023-02-15 21:39:03 +0100

    remove context

--- tf/azure/vm/vm.sh
+++ tf/azure/vm/vm.sh
@@ -34,9 +34,6 @@ sudo groupadd docker
 sudo usermod -aG docker adminuser
 newgrp docker
 
-sudo chown adminuser:adminuser /home/adminuser/.docker/config.json
-sudo chmod 600 /home/adminuser/.docker/config.json
-
 sudo curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
 sudo apt-get install jq --yes
