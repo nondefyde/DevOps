7581e4234f601fff70d80bf4a34ed59a6dda9bd6
Emmanuel Okafor <nondefyde@gmail.com>
2023-02-17 01:44:57 +0100

    deploy images

--- tf/azure/gw/main.tf
+++ tf/azure/gw/main.tf
@@ -11,14 +11,14 @@ resource "azurerm_subnet" "frontend" {
   name                 = "${var.prefix}-frontend-subnet"
   resource_group_name  = data.azurerm_resource_group.rg.name
   virtual_network_name = data.azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.254.0.0/24"]
+  address_prefixes     = [var.address_prefix]
 }
 
 resource "azurerm_subnet" "backend" {
   name                 = "${var.prefix}-backend-subnet"
   resource_group_name  = data.azurerm_resource_group.rg.name
   virtual_network_name = data.azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.254.2.0/24"]
+  address_prefixes     = [var.address_prefix]
 }
 
 resource "azurerm_public_ip" "gw_ip" {
--- tf/azure/gw/variables.tf
+++ tf/azure/gw/variables.tf
@@ -27,3 +27,8 @@ variable "client_secret" {
   type        = string
   description = "Client ID"
 }
+
+variable "address_prefix" {
+  type = string
+  default = "10.0.2.0/24"
+}
