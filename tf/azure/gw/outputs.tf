e708ad692d0621eee76d43d74d03fa650c0862c0
Emmanuel Okafor <nondefyde@gmail.com>
2023-02-17 21:26:08 +0100

    completed app gw

--- tf/azure/gw/outputs.tf
+++ tf/azure/gw/outputs.tf
@@ -1,7 +1,3 @@
 output "gw_name" {
   value = azurerm_application_gateway.gw_network.name
 }
-
-output "backend_address_pools" {
-  value = azurerm_application_gateway.gw_network.backend_address_pool
-}
--- tf/azure/gwc/outputs.tf
+++ tf/azure/gwc/outputs.tf
@@ -2,18 +2,10 @@ output "gw_name" {
   value = data.azurerm_application_gateway.gw_network.name
 }
 
-output "azurerm_network_interfaces" {
-  value = data.azurerm_network_interface.net_interface
+output "backend_pool_name" {
+  value = local.backend_pool.name
 }
 
-output "backend_address_pools" {
-  value = data.azurerm_application_gateway.gw_network.backend_address_pool
-}
-
-output "backend_pool" {
-  value = local.backend_pool
-}
-
-output "network_interface_application_gateway_backend_address_pool_id" {
-  value = azurerm_network_interface_application_gateway_backend_address_pool_association.nic-assoc.*.backend_address_pool_id
+output "backend_pool_id" {
+  value = local.backend_pool.id
 }
