6592ad5310d0926479c90a9995cbe001313f8733
Emmanuel Okafor <nondefyde@gmail.com>
2023-02-18 15:18:58 +0100

    fixed bugs

--- tf/azure/vm/_scripts/vm.sh
+++ tf/azure/vm/_scripts/vm.sh
@@ -6,37 +6,41 @@ while [ "$(hostname -I)" = "" ]; do
 done
 echo "I have network";
 
-# Update the apt package index and install packages to allow apt to use a repository over HTTPS
-sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+if [[ $(which docker) && $(docker --version) ]]; then
+    echo "Installation already done"
+else
+  # Update the apt package index and install packages to allow apt to use a repository over HTTPS
+  sudo apt update
+  sudo apt install apt-transport-https ca-certificates curl software-properties-common
 
-# Add Docker’s official GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  # Add Docker’s official GPG key
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-# The following command is to set up the stable repository
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu `lsb_release -cs` stable"
+  # The following command is to set up the stable repository
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu `lsb_release -cs` stable"
 
-# Update the apt package index, and install the latest version of Docker Engine and contained, or go to the next step to install a specific version
-sudo apt update
-sudo apt install -y docker-ce
+  # Update the apt package index, and install the latest version of Docker Engine and contained, or go to the next step to install a specific version
+  sudo apt update
+  sudo apt install -y docker-ce
 
-#Install Docker compose
-sudo mkdir -p /usr/local/lib/docker/cli-plugins
-sudo curl -SL https://github.com/docker/compose/releases/download/v2.12.2/docker-compose-linux-x86_64 -o /usr/local/lib/docker/cli-plugins/docker-compose
-sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
-sudo docker compose version
+  #Install Docker compose
+  sudo mkdir -p /usr/local/lib/docker/cli-plugins
+  sudo curl -SL https://github.com/docker/compose/releases/download/v2.12.2/docker-compose-linux-x86_64 -o /usr/local/lib/docker/cli-plugins/docker-compose
+  sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
+  sudo docker compose version
 
-sudo docker pull jwilder/nginx-proxy:latest
-sudo docker network create nginx-proxy
-sudo docker volume create app-volume
+  sudo docker pull jwilder/nginx-proxy:latest
+  sudo docker network create nginx-proxy
+  sudo docker volume create app-volume
 
-sudo groupadd docker
-sudo usermod -aG docker adminuser
-newgrp docker
+  sudo groupadd docker
+  sudo usermod -aG docker adminuser
+  newgrp docker
 
-sudo curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+  sudo curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
-sudo apt-get install jq --yes
+  sudo apt-get install jq --yes
+fi
 
 
 
