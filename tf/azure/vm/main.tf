6c9294b3106453a1e998121bd59b58138ea65b82
Emmanuel Okafor <nondefyde@gmail.com>
2023-02-20 00:06:37 +0100

    sku capacity

--- tf/azure/vm/main.tf
+++ tf/azure/vm/main.tf
@@ -7,12 +7,12 @@ data "azurerm_virtual_network" "vnet" {
   resource_group_name = data.azurerm_resource_group.rg.name
 }
 
-data "azurerm_subnet" "subnets" {
-  name                 = data.azurerm_virtual_network.vnet.subnets[count.index]
+data "azurerm_subnet" "vm_subnet" {
+  name                 = "${var.prefix}-subnet"
   virtual_network_name = data.azurerm_virtual_network.vnet.name
   resource_group_name  = data.azurerm_virtual_network.vnet.resource_group_name
-  count                = length(data.azurerm_virtual_network.vnet.subnets)
 }
+
 
 module "az_vm" {
   source = "./modules/az_vm"
@@ -27,7 +27,7 @@ module "az_vm" {
   client_id       = var.client_id
 
   location  = data.azurerm_resource_group.rg.location
-  subnet_id = data.azurerm_subnet.subnets[0].id
+  subnet_id = data.azurerm_subnet.vm_subnet.id
 
   cloud_init_file = var.init_file
   admin_username  = var.admin_username
