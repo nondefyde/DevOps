22c090a5276c5928a800dcce65f1caad22a7dd1d
Emmanuel Okafor <nondefyde@gmail.com>
2023-02-19 15:34:03 +0100

    fixed bugs

--- ci/app.js
+++ ci/app.js
@@ -10,7 +10,7 @@ app.get('/v1/ping', (req, res) => {
 app.get('/', (req, res) => {
 	res.status(200).json({
 		message: `Hello World! Confirm from ${process.env.NODE_ENV}`,
-		request: req
+		headers: req.headers
 	});
 })
 
