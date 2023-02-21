001480b6fa375af7e59e99a27f50138dbdbe8684
Emmanuel Okafor <nondefyde@gmail.com>
2023-02-21 19:51:36 +0100

    sku capacity

--- tf/azure/vnet/outputs.tf
+++ tf/azure/vnet/outputs.tf
@@ -13,11 +13,3 @@ output "virtual_subnet_name" {
 output "virtual_subnet_id" {
   value = azurerm_subnet.app_virtual_subnet.id
 }
-
-output "network_security_group_id" {
-  value = azurerm_network_security_group.vm_security_group.id
-}
-
-output "network_security_group_association_id" {
-  value = azurerm_subnet_network_security_group_association.nsg-assoc.id
-}
