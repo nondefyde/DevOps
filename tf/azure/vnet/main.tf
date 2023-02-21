66e92f0728d098a12a79c71f82a1f8e891ec2ffa
Emmanuel Okafor <nondefyde@gmail.com>
2023-02-21 19:48:12 +0100

    sku capacity

--- tf/azure/vnet/main.tf
+++ tf/azure/vnet/main.tf
@@ -30,30 +30,6 @@ resource "azurerm_subnet" "apim_subnet" {
   address_prefixes     = [var.apim_address_prefix]
 }
 
-resource "azurerm_network_security_group" "vm_security_group" {
-  name                = "${var.prefix}-net-sec-group"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
-
-  security_rule {
-    name                       = "sub-domains"
-    priority                   = 300
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
-
-resource "azurerm_subnet_network_security_group_association" "nsg-assoc" {
-  subnet_id                 = azurerm_subnet.app_virtual_subnet.id
-  network_security_group_id = azurerm_network_security_group.vm_security_group.id
-}
-
-
 resource "azurerm_network_security_group" "apim_security_group" {
   name                = "${var.prefix}-apim-nsg-group"
   location            = data.azurerm_resource_group.rg.location
