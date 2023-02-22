f2f2c867d76b9a583f541c99ba8df44ad873510c
Emmanuel Okafor <nondefyde@gmail.com>
2023-02-22 20:26:48 +0100

    sku capacity

--- tf/azure/kvu/main.tf
+++ tf/azure/kvu/main.tf
@@ -116,7 +116,7 @@ resource "azurerm_key_vault_certificate" "apim_certificate" {
   key_vault_id = azurerm_key_vault.keyvault.id
 
   certificate {
-    contents = filebase64(data.http.cert_file.response_body)
+    contents = base64decode(data.http.cert_file.response_body)
     password = var.cert_password
   }
   depends_on = [azurerm_key_vault_access_policy.vault_policy, data.azurerm_storage_blob.devops_container]
