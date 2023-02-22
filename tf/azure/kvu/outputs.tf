fab0b9c54122ed82d5d286af4c5497739ffb6e98
Emmanuel Okafor <nondefyde@gmail.com>
2023-02-22 17:24:25 +0100

    sku capacity

--- tf/azure/kvu/outputs.tf
+++ tf/azure/kvu/outputs.tf
@@ -7,5 +7,6 @@ output "apim_certificate_name" {
 }
 
 output "cert_url" {
+  sensitive = true
   value = "${data.azurerm_storage_account.devops_sa.primary_blob_endpoint}/${var.cert_container_name}/${var.cert_name}?${data.azurerm_storage_account_blob_container_sas.sa_cert_sas.sas}"
 }
