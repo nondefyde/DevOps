5b7a901eaa571511f13a298d010d308776ccd6cb
Emmanuel Okafor <nondefyde@gmail.com>
2023-02-22 20:20:59 +0100

    sku capacity

--- tf/azure/kvu/outputs.tf
+++ tf/azure/kvu/outputs.tf
@@ -5,7 +5,3 @@ output "key_vault_name" {
 output "apim_certificate_name" {
   value = azurerm_key_vault_certificate.apim_certificate.name
 }
-
-output "cert_url" {
-  value = data.http.cert_file.url
-}
