4665c9b068d2e54db1b722026ac8acec37e899ec
Emmanuel Okafor <nondefyde@gmail.com>
2023-02-23 02:19:11 +0100

    sku capacity

--- terraform/azure/vm/vm.sh
+++ terraform/azure/vm/vm.sh
@@ -21,6 +21,7 @@ sudo apt update
 sudo apt install -y docker-ce
 
 sudo docker pull jwilder/nginx-proxy:latest
+sudo docker volume create app-volume
 sudo docker run -d --name reverse_proxy -p 80:80 -v /var/run/docker.sock:/tmp/docker.sock:ro jwilder/nginx-proxy
 
 #Install Docker compose
