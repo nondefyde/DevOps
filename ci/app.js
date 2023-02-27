df555e44bba80dd3f029af182f4e1b2d72e2681b
Emmanuel Okafor <nondefyde@gmail.com>
2023-02-27 18:13:02 +0100

    fixed ssl

--- ci/app.js
+++ ci/app.js
@@ -1,11 +1,37 @@
 const express = require('express')
-const app = express()
-const port = 8000
+const fs = require("fs");
+const https = require("https");
+const http = require("http");
 
+const app = express()
+
+console.log('process.env.CERT_PATH ::: ', process.env.CERT_PATH);
+const options = {
+	pfx: fs.readFileSync(process.env.CERT_PATH || 'cert/cert.pfx'),
+	passphrase: 'Cloud_1@###'
+};
+
+const httpPort = 8000
+const httpsPort = 4443
+
+http.createServer(app)
+	.listen(httpPort, () => {
+		console.log(`HTTP Example app listening on port >>>>>> update ${httpPort}  --- env --- ${process.env.NODE_ENV}`);
+	});
+
+https.createServer(options, app)
+	.listen(httpsPort, () => {
+		console.log(`HTTPS Example app listening on port >>>>>> update ${httpsPort}  --- env --- ${process.env.NODE_ENV}`);
+	});
+
+app.get('/', function (req, res) {
+	res.header('Content-type', 'text/html');
+	return res.end('<h1>Hello, Secure World!</h1>');
+});
 
 app.get('/v1/ping', (req, res) => {
 	res.status(200).send(`Ping Successful ${new Date().toISOString()} --- environment --- ${process.env.NODE_ENV}`);
-})
+});
 
 app.get('/', (req, res) => {
 	res.status(200).json({
@@ -13,8 +39,4 @@ app.get('/', (req, res) => {
 		port: `Running port - ${process.env.PORT}`,
 		headers: req.headers
 	});
-})
-
-app.listen(port, () => {
-	console.log(`Example app listening on port >>>>>> update ${port}  --- env --- ${process.env.NODE_ENV}`);
-})
+});
--- tf/azure/vm/_scripts/execute.sh
+++ tf/azure/vm/_scripts/execute.sh
@@ -15,6 +15,8 @@ echo "Virtual Host      : ${10}"
 echo "Port Host         : ${11}"
 echo "Environment       : ${12}"
 echo "Container Inst.   : ${13}"
+echo "Cert name"        : ${14}
+echo "Cert vault"       : ${15}
 
 PROJECT=${4}
 IMAGE=${5}
@@ -26,6 +28,8 @@ VIRTUAL_HOST=${10}
 PORT=${11}
 ENV=${12}
 INSTANCE=${13}
+CERT_NAME=${14}
+CERT_VAULT=${15}
 
 PREP_SCRIPT="https://raw.githubusercontent.com/nondefyde/DevOps/main/tf/azure/_scripts/prep.sh"
 
@@ -55,6 +59,17 @@ for i in $(seq 1 ${8}); do
          az login --service-principal --username ${1} --password ${2} --tenant ${3}
       ' \
     --parameters ${1} ${2} ${3}
+
+    echo "Download cert file from Azure Vault VM ${4}-${7}-vm-$INDEX"
+    az vm run-command invoke \
+      --command-id RunShellScript \
+      --name ${4}-${7}-vm-$INDEX \
+      --resource-group ${4}-group \
+      --scripts '
+           mkdir "/home/${3}/vm"
+           az keyvault certificate download --file cert.pfx --name ${2} --vault-name ${1}
+        ' \
+      --parameters ${CERT_VAULT} ${CERT_NAME} ${VM_USER}
 
   echo "Login docker on VM ${4}-${7}-vm-$INDEX"
     az vm run-command invoke \
