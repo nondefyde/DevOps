c46e22eff9a207aacb90ed36fdfebffe7bb4257b
Emmanuel Okafor <nondefyde@gmail.com>
2023-02-28 15:12:11 +0100

    fixed ssl

--- tf/azure/gw/main.tf
+++ tf/azure/gw/main.tf
@@ -286,7 +286,8 @@ resource "azurerm_network_security_group" "gw_security_group" {
     protocol                   = "Tcp"
     source_address_prefix      = "${azurerm_public_ip.gw_ip.ip_address}/32"
     destination_address_prefix = "${data.azurerm_subnet.gw_subnet.address_prefix}"
-    destination_port_range     = "${local.http_frontend_port_name}"
+    source_port_range          = "*"
+    destination_port_range     = "*"
   }
 }
 
