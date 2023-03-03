aa7c95d390064f8876e89741589c4efb829fb3a4
Emmanuel Okafor <nondefyde@gmail.com>
2023-03-03 13:24:48 +0100

    fixed cloud

--- ci/app.js
+++ ci/app.js
@@ -1,17 +1,26 @@
 const express = require('express')
+const os = require('os');
 
 const port = 8000;
 const app = express()
 
-console.log('__dirname ::: ', __dirname);
-console.log('__filename ::: ', __filename);
+const interfaces = os.networkInterfaces();
+const containerInterface = Object.values(interfaces)
+	.flat()
+	.find(iface => iface.internal === false && iface.mac === '02:42:ac:11:00:02');
+console.log('containerInterface ::: ', containerInterface);
+let containerIp;
+if(containerInterface) {
+	containerIp = containerInterface.address;
+}
 
 app.listen(port, () => {
 	console.log(`HTTP app listening on port >>>> ${port}`);
 });
 
 app.get('/v1/ping', (req, res) => {
-	res.status(200).send(`Ping Successful ${new Date().toISOString()} --- environment --- ${process.env.NODE_ENV}`);
+	const containerIp = containerInterface.address;
+	res.status(200).send(`Ping Successful ${new Date().toISOString()} --- containerIp --- ${containerIp}`);
 });
 
 app.get('/', (req, res) => {
@@ -19,7 +28,7 @@ app.get('/', (req, res) => {
 		message: `Hello from ${process.env.NODE_ENV}`,
 		headers: req.headers,
 		params: req.params,
-		body: req.body,
+		containerIp
 	});
 });
 
@@ -28,6 +37,6 @@ app.get('/:url', (req, res) => {
 		message: `Hello from url ${process.env.NODE_ENV}`,
 		headers: req.headers,
 		params: req.params,
-		body: req.body,
+		containerIp
 	});
 });
