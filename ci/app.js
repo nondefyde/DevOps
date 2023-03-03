3a9d38d7589c6de1984517e4eac2127b2bdd62df
Emmanuel Okafor <nondefyde@gmail.com>
2023-03-03 19:44:18 +0100

    fixed cloud

--- ci/app.js
+++ ci/app.js
@@ -1,26 +1,14 @@
 const express = require('express')
-const os = require('os');
 
 const port = 8000;
 const app = express()
-
-const interfaces = os.networkInterfaces();
-const containerInterface = Object.values(interfaces)
-	.flat()
-	.find(iface => iface.internal === false && iface.mac === '02:42:ac:11:00:02');
-console.log('containerInterface ::: ', containerInterface);
-let containerIp;
-if(containerInterface) {
-	containerIp = containerInterface.address;
-}
 
 app.listen(port, () => {
 	console.log(`HTTP app listening on port >>>> ${port}`);
 });
 
 app.get('/ping', (req, res) => {
-	const containerIp = containerInterface.address;
-	res.status(200).send(`Ping Successful ${new Date().toISOString()} --- containerIp --- ${containerIp}`);
+	res.status(200).send(`Ping Successful ${new Date().toISOString()}`);
 });
 
 app.get('/', (req, res) => {
@@ -28,7 +16,6 @@ app.get('/', (req, res) => {
 		message: `Hello from ${process.env.NODE_ENV}`,
 		headers: req.headers,
 		params: req.params,
-		containerIp
 	});
 });
 
@@ -37,6 +24,5 @@ app.get('/:url', (req, res) => {
 		message: `Hello from url ${process.env.NODE_ENV}`,
 		headers: req.headers,
 		params: req.params,
-		containerIp
 	});
 });
