1c0df8525cbbf4a2acf209fdcae56ff135757b80
Emmanuel Okafor <nondefyde@gmail.com>
2023-03-03 22:58:56 +0100

    added required headers

--- ci/app.js
+++ ci/app.js
@@ -7,21 +7,9 @@ app.listen(port, () => {
 	console.log(`HTTP app listening on port >>>> ${port}`);
 });
 
-app.get('/ping', (req, res) => {
-	res.status(200).send(`Ping Successful from ${process.env.NODE_ENV}`);
-});
-
 app.get('/', (req, res) => {
 	res.status(200).json({
-		message: `Hello from ${process.env.NODE_ENV}`,
-		headers: req.headers,
-		params: req.params,
-	});
-});
-
-app.get('/:url', (req, res) => {
-	res.status(200).json({
-		message: `Hello from url ${process.env.NODE_ENV}`,
+		message: `Ping Successful from ${process.env.NODE_ENV}`,
 		headers: req.headers,
 		params: req.params,
 	});
