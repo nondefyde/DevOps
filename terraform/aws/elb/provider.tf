e13105da89e8db6a55a4b2f14cf9e3ebcd67f047
Emmanuel Okafor <nondefyde@gmail.com>
2023-05-07 22:09:24 +0100

    added aws scripts

--- terraform/aws/elb/provider.tf
+++ terraform/aws/elb/provider.tf
@@ -14,3 +14,25 @@ provider "aws" {
   access_key = var.aws_key_id
   secret_key = var.aws_key_secret
 }
+
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+provider "kubectl" {
+  host                   = data.aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.eks.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
