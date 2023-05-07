ef8f4cef8e1e5bf7fccdf50e2e04839f355424cf
Emmanuel Okafor <nondefyde@gmail.com>
2023-05-07 20:10:58 +0100

    added aws scripts

--- terraform/aws/elb/outputs.tf
+++ terraform/aws/elb/outputs.tf
@@ -1,19 +1,6 @@
-output "public_ip_address" {
-  value = var.app_project_prefix
-}
-
-output "region" {
-  description = "AWS region"
-  value       = var.aws_region
-}
-
 output "cluster_name" {
   description = "Kubernetes Cluster Name"
   value       = local.eks_cluster_name
-}
-
-output "account_id" {
-  value = data.aws_caller_identity.current.account_id
 }
 
 output "caller_arn" {
@@ -34,8 +21,4 @@ output "caller_id" {
 
 output "issuer" {
   value = data.aws_eks_cluster.eks.identity.0.oidc.0.issuer
-}
-
-output "default_secret_name" {
-  value = module.elb.default_secret_name
 }
