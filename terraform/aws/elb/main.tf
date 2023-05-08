03ea5b7f6554926adb247f81f5b3481c9edf3e0c
Emmanuel Okafor <nondefyde@gmail.com>
2023-05-08 14:36:40 +0100

    added policy

--- terraform/aws/elb/main.tf
+++ terraform/aws/elb/main.tf
@@ -37,40 +37,24 @@ module "nginx-controller" {
       value = "true"
       type  = "string"
     },
-#    {
-#      name  = "controller.serviceAccount.create"
-#      value = "false"
-#      type  = "string"
-#    },
-#    {
-#      name  = "controller.serviceAccountName"
-#      value = "aws-load-balancer-controller"
-#      type  = "string"
-#    }
+    {
+      name  = "clusterName"
+      value = local.eks_cluster_name
+      type  = "string"
+    },
+    {
+      name  = "controller.serviceAccount.create"
+      value = "false"
+      type  = "string"
+    },
+    {
+      name  = "controller.serviceAccountName"
+      value = "aws-load-balancer-controller"
+      type  = "string"
+    }
   ]
 
   depends_on = [
     module.elb
   ]
 }
-
-#
-#module "nginx-controller" {
-#  source  = "terraform-iaac/nginx-controller/helm"
-#  additional_set = [
-#    {
-#      name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
-#      value = "nlb"
-#      type  = "string"
-#    },
-#    {
-#      name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-cross-zone-load-balancing-enabled"
-#      value = "true"
-#      type  = "string"
-#    }
-#  ]
-#
-#  depends_on = [
-#    aws_ecr_repository.app_registry
-#  ]
-#}
