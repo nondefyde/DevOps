c3b266f72f3c81e2679566e3b78360b9d5ec2b34
Emmanuel Okafor <nondefyde@gmail.com>
2023-05-09 00:40:16 +0100

    added policy

--- terraform/aws/elb/main.tf
+++ terraform/aws/elb/main.tf
@@ -12,6 +12,28 @@ resource "aws_iam_openid_connect_provider" "oidc" {
   url             = data.aws_eks_cluster.eks.identity.0.oidc.0.issuer
 }
 
+resource "null_resource" "update-kubeconfig" {
+  provisioner "local-exec" {
+    command = "aws eks update-kubeconfig --name ${var.cluster_name} --region ${var.aws_region}"
+  }
+
+  depends_on = [
+    aws_iam_openid_connect_provider.oidc,
+    aws_iam_policy.elb-policy
+  ]
+}
+
+resource "null_resource" "associate_iam_oidc_provider" {
+  provisioner "local-exec" {
+    command = "eksctl utils associate-iam-oidc-provider --region=${var.aws_region} --cluster=${var.cluster_name} --approve"
+  }
+
+  depends_on = [
+    null_resource.update-kubeconfig,
+    aws_iam_policy.elb-policy
+  ]
+}
+
 module "elb" {
   source       = "./modules/elb"
   project      = var.app_project_prefix
@@ -21,6 +43,7 @@ module "elb" {
   account_id   = data.aws_caller_identity.current.account_id
   aws_region   = var.aws_region
 }
+
 #
 #module "nginx" {
 #  source       = "./modules/nginx"
