ec7c05e4406b8f7634b41943dc9f467232754946
Emmanuel Okafor <nondefyde@gmail.com>
2023-05-08 16:34:46 +0100

    added policy

--- terraform/aws/eks/modules/eks/main.tf
+++ terraform/aws/eks/modules/eks/main.tf
@@ -1,6 +1,7 @@
 resource "aws_eks_cluster" "eks" {
   name     = var.cluster_name
   role_arn = aws_iam_role.eks-iam-role.arn
+  version  = "1.24"
 
   vpc_config {
     subnet_ids              = var.subnet_ids
--- terraform/aws/elb/main.tf
+++ terraform/aws/elb/main.tf
@@ -13,20 +13,9 @@ module "elb" {
 }
 
 module "nginx-controller" {
-  source         = "terraform-iaac/nginx-controller/helm"
-#  atomic = true
-#    wait         = false
+  source  = "terraform-iaac/nginx-controller/helm"
+  atomic = true
   additional_set = [
-    {
-      name  = "region"
-      value = var.aws_region
-      type  = "string"
-    },
-    {
-      name  = "vpcId"
-      value = data.aws_eks_cluster.eks.vpc_config[0].vpc_id
-      type  = "string"
-    },
     {
       name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
       value = "nlb"
@@ -36,21 +25,6 @@ module "nginx-controller" {
       name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-cross-zone-load-balancing-enabled"
       value = "true"
       type  = "string"
-    },
-    {
-      name  = "clusterName"
-      value = local.eks_cluster_name
-      type  = "string"
-    },
-    {
-      name  = "controller.serviceAccount.create"
-      value = "false"
-      type  = "string"
-    },
-    {
-      name  = "controller.serviceAccountName"
-      value = "aws-load-balancer-controller"
-      type  = "string"
     }
   ]
 
@@ -58,3 +32,120 @@ module "nginx-controller" {
     module.elb
   ]
 }
+
+#resource "kubernetes_manifest" "nginx" {
+#  manifest = file("./yamls/nginx.yaml")
+#}
+#
+#resource "kubernetes_service_account" "nginx-ingress" {
+#  metadata {
+#    name      = "nginx-ingress"
+#    namespace = "kube-system"
+#  }
+#}
+#
+#resource "helm_release" "nginx-ingress" {
+#  name       = "nginx-ingress"
+#  namespace  = "kube-system"
+#  repository = "https://kubernetes-charts.storage.googleapis.com/"
+#  chart      = "nginx-ingress"
+#  version    = "3.36.0"
+#
+#  set {
+#    name  = "controller.service.type"
+#    value = "LoadBalancer"
+#  }
+#
+#  set {
+#    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-internal"
+#    value = "true"
+#  }
+#
+#  set {
+#    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-backend-protocol"
+#    value = "http"
+#  }
+#
+#  set {
+#    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-ssl-ports"
+#    value = "https"
+#  }
+#
+#  set {
+#    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-ssl-cert"
+#    value = "arn:aws:acm:us-west-2:123456789012:certificate/abcde12345-6789-0123-4567-89abcdef0123"
+#  }
+#
+#  set {
+#    name  = "controller.publishService.enabled"
+#    value = "false"
+#  }
+#
+#  set {
+#    name  = "controller.replicaCount"
+#    value = "2"
+#  }
+#
+#  set {
+#    name  = "defaultBackend.enabled"
+#    value = "false"
+#  }
+#
+#  set {
+#    name  = "rbac.create"
+#    value = "true"
+#  }
+#
+#  set {
+#    name  = "rbac.serviceAccountName"
+#    value = kubernetes_service_account.nginx-ingress.metadata.0.name
+#  }
+#}
+#
+#
+#module "nginx-controller" {
+#  source         = "terraform-iaac/nginx-controller/helm"
+##  atomic = true
+##    wait         = false
+#  additional_set = [
+#    {
+#      name  = "region"
+#      value = var.aws_region
+#      type  = "string"
+#    },
+#    {
+#      name  = "vpcId"
+#      value = data.aws_eks_cluster.eks.vpc_config[0].vpc_id
+#      type  = "string"
+#    },
+#    {
+#      name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
+#      value = "nlb"
+#      type  = "string"
+#    },
+#    {
+#      name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-cross-zone-load-balancing-enabled"
+#      value = "true"
+#      type  = "string"
+#    },
+#    {
+#      name  = "clusterName"
+#      value = local.eks_cluster_name
+#      type  = "string"
+#    },
+#    {
+#      name  = "controller.serviceAccount.create"
+#      value = "false"
+#      type  = "string"
+#    },
+#    {
+#      name  = "controller.serviceAccountName"
+#      value = "aws-load-balancer-controller"
+#      type  = "string"
+#    }
+#  ]
+#
+#  depends_on = [
+#    module.elb
+#  ]
+#}
