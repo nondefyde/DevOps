cf38dbcfbe99c20415e6d19e0f0f1885103f17c0
Emmanuel Okafor <nondefyde@gmail.com>
2023-05-09 00:53:19 +0100

    added policy

--- terraform/aws/elb/main.tf
+++ terraform/aws/elb/main.tf
@@ -32,8 +32,8 @@ resource "null_resource" "associate_iam_oidc_provider" {
   ]
 }
 
-module "elb" {
-  source       = "modules/elb"
+module "alb" {
+  source       = "./modules/alb"
   project      = var.app_project_prefix
   vpc_id       = data.aws_eks_cluster.eks.vpc_config[0].vpc_id
   cluster_name = data.aws_eks_cluster.eks.name
@@ -55,6 +55,6 @@ module "nginx" {
   account_id   = data.aws_caller_identity.current.account_id
   aws_region   = var.aws_region
   depends_on = [
-    module.elb
+    module.alb
   ]
 }
