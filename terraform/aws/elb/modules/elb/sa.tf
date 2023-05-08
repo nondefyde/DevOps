a3fba19062959848e8db001f082b4375490ed099
Emmanuel Okafor <nondefyde@gmail.com>
2023-05-08 13:49:32 +0100

    added policy

--- terraform/aws/elb/modules/elb/sa.tf
+++ terraform/aws/elb/modules/elb/sa.tf
@@ -21,6 +21,26 @@ resource "aws_iam_role" "role_service_account" {
   })
 }
 
+resource "aws_iam_role_policy_attachment" "eks-iam-role-1-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.role_service_account.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks-iam-role-1-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.role_service_account.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks-iam-role-1-EC2InstanceProfileForImageBuilderECRContainerBuilds" {
+  policy_arn = "arn:aws:iam::aws:policy/EC2InstanceProfileForImageBuilderECRContainerBuilds"
+  role    = aws_iam_role.role_service_account.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks-iam-role-1-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.role_service_account.name
+}
+
 resource "aws_iam_policy" "elb-policy" {
   name        = "${var.project}-aws-elb-policy"
   path        = "/"
