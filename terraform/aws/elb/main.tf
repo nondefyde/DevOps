11a3807e1403b85c8ce5686c854d2b2f3468b4ff
Emmanuel Okafor <nondefyde@gmail.com>
2023-05-09 00:45:47 +0100

    added policy

--- terraform/aws/elb/main.tf
+++ terraform/aws/elb/main.tf
@@ -14,28 +14,26 @@ resource "aws_iam_openid_connect_provider" "oidc" {
 
 resource "null_resource" "update-kubeconfig" {
   provisioner "local-exec" {
-    command = "aws eks update-kubeconfig --name ${var.cluster_name} --region ${var.aws_region}"
+    command = "aws eks update-kubeconfig --name ${local.eks_cluster_name} --region ${var.aws_region}"
   }
 
   depends_on = [
-    aws_iam_openid_connect_provider.oidc,
-    aws_iam_policy.elb-policy
+    aws_iam_openid_connect_provider.oidc
   ]
 }
 
 resource "null_resource" "associate_iam_oidc_provider" {
   provisioner "local-exec" {
-    command = "eksctl utils associate-iam-oidc-provider --region=${var.aws_region} --cluster=${var.cluster_name} --approve"
+    command = "eksctl utils associate-iam-oidc-provider --region=${var.aws_region} --cluster=${local.eks_cluster_name} --approve"
   }
 
   depends_on = [
-    null_resource.update-kubeconfig,
-    aws_iam_policy.elb-policy
+    null_resource.update-kubeconfig
   ]
 }
 
-module "elb" {
-  source       = "./modules/elb"
+module "alb" {
+  source       = "modules/alb"
   project      = var.app_project_prefix
   vpc_id       = data.aws_eks_cluster.eks.vpc_config[0].vpc_id
   cluster_name = data.aws_eks_cluster.eks.name
@@ -48,13 +46,15 @@ module "elb" {
   ]
 }
 
-#
-#module "nginx" {
-#  source       = "./modules/nginx"
-#  project      = var.app_project_prefix
-#  vpc_id       = data.aws_eks_cluster.eks.vpc_config[0].vpc_id
-#  cluster_name = data.aws_eks_cluster.eks.name
-#  issuer       = data.aws_eks_cluster.eks.identity.0.oidc.0.issuer
-#  account_id   = data.aws_caller_identity.current.account_id
-#  aws_region   = var.aws_region
-#}
+module "nginx" {
+  source       = "./modules/nginx"
+  project      = var.app_project_prefix
+  vpc_id       = data.aws_eks_cluster.eks.vpc_config[0].vpc_id
+  cluster_name = data.aws_eks_cluster.eks.name
+  issuer       = data.aws_eks_cluster.eks.identity.0.oidc.0.issuer
+  account_id   = data.aws_caller_identity.current.account_id
+  aws_region   = var.aws_region
+  depends_on = [
+    module.elb
+  ]
+}
