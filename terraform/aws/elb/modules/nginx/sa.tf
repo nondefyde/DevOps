96476b04a08b9b8c82043bac0b3cd2368987ed49
Emmanuel Okafor <nondefyde@gmail.com>
2023-05-09 01:07:41 +0100

    added policy

--- terraform/aws/elb/modules/nginx/sa.tf
+++ terraform/aws/elb/modules/nginx/sa.tf
@@ -21,6 +21,32 @@ resource "aws_iam_role" "role_service_account" {
   })
 }
 
+resource "aws_iam_role_policy_attachment" "eks-iam-role-1-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.role_service_account.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks-iam-role-1-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.role_service_account.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks-iam-role-1-EC2InstanceProfileForImageBuilderECRContainerBuilds" {
+  policy_arn = "arn:aws:iam::aws:policy/EC2InstanceProfileForImageBuilderECRContainerBuilds"
+  role    = aws_iam_role.role_service_account.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks-iam-role-1-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.role_service_account.name
+}
+
+
+resource "aws_iam_role_policy_attachment" "eks-iam-role-1-AmazonSSMManagedInstanceCore" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  role       = aws_iam_role.role_service_account.name
+}
+
 resource "kubernetes_service_account" "service_account" {
   metadata {
     name = var.sa_name
